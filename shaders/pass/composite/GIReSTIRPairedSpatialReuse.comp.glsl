fa99ae7bc5b2f0bdd0fc34c5403b919e748a8bc4
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-05-15 05:26:39 -0400

    Paired reuse clean

--- shaders/pass/composite/GIReSTIRPairedSpatialReuse.comp.glsl
+++ shaders/pass/composite/GIReSTIRPairedSpatialReuse.comp.glsl
@@ -54,10 +54,10 @@ bool shiftMapping_isReusable(ShiftMapping mapping) {
 }
 
 ShiftMapping evaluateShiftMapping(
-    ReSTIRReservoir canonResSRC,
-    Material matDST,
-    SpatialSampleData sampleDST, SpatialSampleData sampleSRC,
-    vec3 viewPosDST, vec3 viewPosSRC
+ReSTIRReservoir canonResSRC,
+Material matDST,
+SpatialSampleData sampleDST, SpatialSampleData sampleSRC,
+vec3 viewPosDST, vec3 viewPosSRC
 ) {
     ShiftMapping mapping = shiftMapping_init();
 
@@ -87,12 +87,12 @@ ShiftMapping evaluateShiftMapping(
 }
 
 void applyShiftMapping(
-    ivec2 texelDST, ivec2 texelSRC,
-    inout ReSTIRReservoir accumResDST,
-    ReSTIRReservoir canonResDST, ReSTIRReservoir canonResSRC,
-    inout uvec4 metaDST,
-    SpatialSampleData sampleDST, SpatialSampleData sampleSRC,
-    ShiftMapping srcToDst, ShiftMapping dstToSrc
+ivec2 texelDST, ivec2 texelSRC,
+inout ReSTIRReservoir accumResDST,
+ReSTIRReservoir canonResDST, ReSTIRReservoir canonResSRC,
+inout uvec4 metaDST,
+SpatialSampleData sampleDST, SpatialSampleData sampleSRC,
+ShiftMapping srcToDst, ShiftMapping dstToSrc
 ) {
     if (shiftMapping_isReusable(srcToDst)) {
         float rcMDivK_DST = canonResDST.m / 8.0;
@@ -135,64 +135,80 @@ void main() {
     ivec2 texelB = tileOrigin + localB;
     bool validA = all(lessThan(ivec4(texelA, -1, -1), ivec4(uval_mainImageSizeI, texelA)));
     bool validB = all(lessThan(ivec4(texelB, -1, -1), ivec4(uval_mainImageSizeI, texelB)));
-    if (!validA || !validB || texelA == texelB) return;
 
-    Material matA, matB;
-    SpatialSampleData sampleA, sampleB;
-    vec3 viewPosA, viewPosB;
-    float viewZA, viewZB;
-    ReSTIRReservoir canonResA, canonResB;
     ReSTIRReservoir accumResA = restir_initReservoir();
     ReSTIRReservoir accumResB = restir_initReservoir();
-    uvec4 metaA = uvec4(0), metaB = uvec4(0);
-    GBufferData gDataA = gbufferData_init();
-    gbufferData1_unpack(texelFetch(usam_gbufferSolidData1, texelA, 0), gDataA);
-    gbufferData2_unpack(texelFetch(usam_gbufferSolidData2, texelA, 0), gDataA);
-    matA = material_decode(gDataA);
-    sampleA = spatialSampleData_unpack(transient_restir_spatialInput_fetch(texelA));
-    viewZA = texelFetch(usam_gbufferSolidViewZ, texelA, 0).x;
-    if (viewZA <= -65536.0) return;
-    vec2 screenPosA = coords_texelToUV(texelA, uval_mainImageSizeRcp);
-    viewPosA = coords_toViewCoord(screenPosA, viewZA, global_camProjInverse);
-    uvec4 repA = bool(frameCounter & 1) ? history_restir_reservoirTemporal1_fetch(texelA) : history_restir_reservoirTemporal2_fetch(texelA);
-    canonResA = restir_reservoir_unpack(repA);
-    #if PASS_INDEX == 0
-    accumResA = canonResA;
-    metaA = uvec4((uint(texelA.y) << 16) | uint(texelA.x), 0u, floatBitsToUint(1.0), 0u);
-    #else
-    accumResA = restir_reservoir_unpack(transient_restir_spatialReservoirAccum_load(texelA));
-    metaA = transient_restir_pairwiseMISMetadata_load(texelA);
-    #endif
 
-    GBufferData gDataB = gbufferData_init();
-    gbufferData1_unpack(texelFetch(usam_gbufferSolidData1, texelB, 0), gDataB);
-    gbufferData2_unpack(texelFetch(usam_gbufferSolidData2, texelB, 0), gDataB);
-    matB = material_decode(gDataB);
-    sampleB = spatialSampleData_unpack(transient_restir_spatialInput_fetch(texelB));
-    viewZB = texelFetch(usam_gbufferSolidViewZ, texelB, 0).x;
-    if (viewZB <= -65536.0) return;
-    vec2 screenPosB = coords_texelToUV(texelB, uval_mainImageSizeRcp);
-    viewPosB = coords_toViewCoord(screenPosB, viewZB, global_camProjInverse);
-    uvec4 repB = bool(frameCounter & 1) ? history_restir_reservoirTemporal1_fetch(texelB) : history_restir_reservoirTemporal2_fetch(texelB);
-    canonResB = restir_reservoir_unpack(repB);
+    uvec4 metaA = uvec4(0);
+    uvec4 metaB = uvec4(0);
     #if PASS_INDEX == 0
-    accumResB = canonResB;
+    metaA = uvec4((uint(texelA.y) << 16) | uint(texelA.x), 0u, floatBitsToUint(1.0), 0u);
     metaB = uvec4((uint(texelB.y) << 16) | uint(texelB.x), 0u, floatBitsToUint(1.0), 0u);
     #else
+    accumResA = restir_reservoir_unpack(transient_restir_spatialReservoirAccum_load(texelA));
     accumResB = restir_reservoir_unpack(transient_restir_spatialReservoirAccum_load(texelB));
+    metaA = transient_restir_pairwiseMISMetadata_load(texelA);
     metaB = transient_restir_pairwiseMISMetadata_load(texelB);
     #endif
 
-    if (dot(sampleA.geomNormal, sampleB.geomNormal) > 0.99) {
-        ShiftMapping shiftAtoB = evaluateShiftMapping(canonResA, matB, sampleB, sampleA, viewPosB, viewPosA);
-        ShiftMapping shiftBtoA = evaluateShiftMapping(canonResB, matA, sampleA, sampleB, viewPosA, viewPosB);
-        applyShiftMapping(texelA, texelB, accumResA, canonResA, canonResB, metaA, sampleA, sampleB, shiftBtoA, shiftAtoB);
-        applyShiftMapping(texelB, texelA, accumResB, canonResB, canonResA, metaB, sampleB, sampleA, shiftAtoB, shiftBtoA);
+    if (validA && validB && texelA != texelB){
+        float viewZA = texelFetch(usam_gbufferSolidViewZ, texelA, 0).x;
+        float viewZB = texelFetch(usam_gbufferSolidViewZ, texelB, 0).x;
+        if (viewZA > -65536.0 && viewZB > -65536.0) {
+            uvec4 spatialSamplePackedDataA = transient_restir_spatialInput_fetch(texelA);
+            uvec4 spatialSamplePackedDataB = transient_restir_spatialInput_fetch(texelB);
+            uvec4 gbufferSolidData1A = texelFetch(usam_gbufferSolidData1, texelA, 0);
+            uvec4 gbufferSolidData1B = texelFetch(usam_gbufferSolidData1, texelB, 0);
+            uvec4 gbufferSolidData2A = texelFetch(usam_gbufferSolidData2, texelA, 0);
+            uvec4 gbufferSolidData2B = texelFetch(usam_gbufferSolidData2, texelB, 0);
+            uvec4 repA;
+            uvec4 repB;
+            if (bool(frameCounter & 1)) {
+                repA = history_restir_reservoirTemporal1_fetch(texelA);
+                repB = history_restir_reservoirTemporal1_fetch(texelB);
+            } else {
+                repA = history_restir_reservoirTemporal2_fetch(texelA);
+                repB = history_restir_reservoirTemporal2_fetch(texelB);
+            }
+
+            SpatialSampleData sampleA = spatialSampleData_unpack(spatialSamplePackedDataA);
+            SpatialSampleData sampleB = spatialSampleData_unpack(spatialSamplePackedDataB);
+
+            GBufferData gDataA = gbufferData_init();
+            gbufferData1_unpack(gbufferSolidData1A, gDataA);
+            gbufferData2_unpack(gbufferSolidData2A, gDataA);
+            Material matA = material_decode(gDataA);
+
+            GBufferData gDataB = gbufferData_init();
+            gbufferData1_unpack(gbufferSolidData1B, gDataB);
+            gbufferData2_unpack(gbufferSolidData2B, gDataB);
+            Material matB = material_decode(gDataB);
+
+            ReSTIRReservoir canonResA = restir_reservoir_unpack(repA);
+            ReSTIRReservoir canonResB = restir_reservoir_unpack(repB);
+
+            #if PASS_INDEX == 0
+            accumResA = canonResA;
+            accumResB = canonResB;
+            #endif
+
+            if (dot(sampleA.geomNormal, sampleB.geomNormal) > 0.99) {
+                vec2 screenPosA = coords_texelToUV(texelA, uval_mainImageSizeRcp);
+                vec3 viewPosA = coords_toViewCoord(screenPosA, viewZA, global_camProjInverse);
+                vec2 screenPosB = coords_texelToUV(texelB, uval_mainImageSizeRcp);
+                vec3 viewPosB = coords_toViewCoord(screenPosB, viewZB, global_camProjInverse);
+
+                ShiftMapping shiftAtoB = evaluateShiftMapping(canonResA, matB, sampleB, sampleA, viewPosB, viewPosA);
+                ShiftMapping shiftBtoA = evaluateShiftMapping(canonResB, matA, sampleA, sampleB, viewPosA, viewPosB);
+                applyShiftMapping(texelA, texelB, accumResA, canonResA, canonResB, metaA, sampleA, sampleB, shiftBtoA, shiftAtoB);
+                applyShiftMapping(texelB, texelA, accumResB, canonResB, canonResA, metaB, sampleB, sampleA, shiftAtoB, shiftBtoA);
+            }
+        }
     }
 
-    transient_restir_spatialReservoirAccum_store(texelA, restir_reservoir_pack(accumResA));
     transient_restir_pairwiseMISMetadata_store(texelA, metaA);
-
-    transient_restir_spatialReservoirAccum_store(texelB, restir_reservoir_pack(accumResB));
     transient_restir_pairwiseMISMetadata_store(texelB, metaB);
+
+    transient_restir_spatialReservoirAccum_store(texelA, restir_reservoir_pack(accumResA));
+    transient_restir_spatialReservoirAccum_store(texelB, restir_reservoir_pack(accumResB));
 }
