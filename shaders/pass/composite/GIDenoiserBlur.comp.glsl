7f6722fd91454c89680583437ef5783a46c502cb
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-05-15 07:20:40 -0400

    x2

--- shaders/pass/composite/GIDenoiserBlur.comp.glsl
+++ shaders/pass/composite/GIDenoiserBlur.comp.glsl
@@ -3,7 +3,7 @@
 #include "/Base.glsl"
 
 #define GI_DENOISE_PASS 1
-#define GI_DENOISE_SAMPLES 0
+#define GI_DENOISE_SAMPLES SETTING_DENOISER_SPATIAL_SAMPLES
 // X: history length radius scale
 // Y: variance heuristic radius scale
 // Z: min radius
